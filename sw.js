72ce0316ee6bab016e7f85a19aedc7d4d2de0eac
FAN-ZY <51874622+FAN-ZY@users.noreply.github.com>
2020-06-17 20:18:20 +0800

    Site updated: 2020-06-17 20:18:18

--- sw.js
+++ sw.js
@@ -21,7 +21,7 @@ self.addEventListener('active', function () {
 });
 
 var precacheUrls = [
-'/','/posts/undefined/','/posts/16107/',
+'/','/posts/61167/','/posts/16107/',
 ];
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":5};
